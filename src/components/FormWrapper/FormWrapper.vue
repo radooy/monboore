d10281148c3848d69b58071aab9cc55b95d1d068
Radostin Kirilov <radostin.kirilov@elando.bg>
2023-01-17 12:41:47 +0200

    Extract a form-wrapper component

---
+++ src/components/FormWrapper/FormWrapper.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-sheet
+    elevation="5"
+    rounded
+    class="w-50 mx-auto my-0"
+    :class="{ 'w-100': $vuetify.display.xs }"
+  >
+    <slot></slot>
+  </v-sheet>
+</template>
