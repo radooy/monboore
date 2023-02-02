d30fd2988458e6d0ec72db875a05f6395b3f50f1
Radostin Kirilov <radostin.kirilov@elando.bg>
2023-02-02 18:29:30 +0200

    implement login

---
+++ src/helpers/enums/login/login.enum.ts
@@ -0,0 +1,17 @@
+export enum PageTexts {
+  logInToProfile = "Влезте в профила си",
+  email = "E-mail",
+  password = "Парола",
+  logIn = "Вход",
+  dontHaveAnAccount = "Нямате акаунт? Създайте своя от тук!",
+}
+
+export enum Errors {
+  emailIsRequired = "Полето E-mail е задължително",
+  emailShouldBeValid = "Моля въведете валиден имейл",
+  passwordIsRequired = "Полето парола е задължително",
+  userNotFound = "Потребител с този имейл не съществува",
+  passwordIsWrong = "Грешна парола",
+  userNotFoundCode = "auth/user-not-found",
+  passwordIsWrongCode = "auth/wrong-password",
+}
--- src/views/LoginView/LoginView.vue
+++ src/views/LoginView/LoginView.vue
@@ -1,6 +1,9 @@
 <template>
   <form-wrapper>
-    <h2 class="text-center mb-3 font-weight-light pt-5">Влезте в профила си</h2>
+    <h2 class="text-center mb-3 font-weight-light pt-5">
+      {{ PageTexts.logInToProfile }}
+    </h2>
+    <ErrorMessage v-if="hasError" :message="errorText" />
     <v-form
       v-model="valid"
       class="w-75 mx-auto my-0 pa-5"
@@ -9,26 +12,27 @@
     >
       <v-text-field
         v-model="email"
-        label="E-mail"
+        :label="PageTexts.email"
         :rules="emailRules"
         required
+        @focus="hasError = false"
       ></v-text-field>
 
       <v-text-field
         v-model="password"
-        label="Парола"
+        :label="PageTexts.password"
         type="password"
         :rules="passRules"
         required
+        @focus="hasError = false"
       ></v-text-field>
-
       <div class="text-center">
-        <v-btn @click="login">Вход</v-btn>
+        <v-btn @click="login" :disabled="!valid">{{ PageTexts.logIn }}</v-btn>
       </div>
     </v-form>
     <div class="text-center pb-5">
-      <RouterLink to="register">
-        Нямате акаунт? Създайте своя от тук!
+      <RouterLink to="register" class="text-grey-darken-1">
+        {{ PageTexts.dontHaveAnAccount }}
       </RouterLink>
     </div>
   </form-wrapper>
@@ -36,23 +40,72 @@
 
 <script lang="ts" setup>
 import { ref } from "vue";
+import { useRouter } from "vue-router";
+import store from "@/store";
+
 import FormWrapper from "@/components/FormWrapper/FormWrapper.vue";
+import ErrorMessage from "@/components/ErrorMessage/ErrorMessage.vue";
+
+import { auth, db } from "@/firebase/index";
+import { signInWithEmailAndPassword, UserCredential } from "firebase/auth";
+import { doc, getDoc } from "@firebase/firestore";
+
+import { PageTexts, Errors } from "@/helpers/enums/login/login.enum";
+import { DbTables } from "@/helpers/enums/db/db.enum";
+import { Actions } from "@/helpers/enums/store/store.enum";
 
 const form = ref<HTMLFormElement>();
+const router = useRouter();
 
 const email = ref("");
 const emailRules = [
-  (v: string) => !!v || "Полето E-mail е задължително",
-  (v: string) => /.+@.+\..+/.test(v) || "Моля въведете валиден имейл",
+  (v: string) => !!v || Errors.emailIsRequired,
+  (v: string) => /.+@.+\..+/.test(v) || Errors.emailShouldBeValid,
 ];
 
 const password = ref("");
-const passRules = [(v: string) => !!v || "Полето парола е задължително"];
+const passRules = [(v: string) => !!v || Errors.passwordIsRequired];
 const valid = ref(true);
+
+const hasError = ref(false);
+const errorText = ref("");
 
 async function login() {
   const { valid } = await form?.value?.validate();
 
-  if (valid) alert("Form is valid");
+  if (valid) {
+    signInWithEmailAndPassword(auth, email.value, password.value)
+      .then(async (userCredential: UserCredential) => {
+        const docRef = doc(db, DbTables.users, userCredential.user.uid);
+
+        try {
+          const docRefSnapshot = await getDoc(docRef);
+
+          if (docRefSnapshot.exists()) {
+            const data = docRefSnapshot.data();
+            const isAdmin: boolean = data.isAdmin;
+            const { email, uid } = userCredential.user;
+
+            store
+              .dispatch(Actions.logIn, { email, uid, isAdmin })
+              .then(() => router.push("/"))
+              .catch((e) => console.log(e));
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      })
+      .catch((e) => {
+        hasError.value = true;
+        if (e.code === Errors.userNotFoundCode) {
+          errorText.value = Errors.userNotFound;
+          return;
+        }
+        if (e.code === Errors.passwordIsWrongCode) {
+          errorText.value = Errors.passwordIsWrong;
+          return;
+        }
+      });
+  }
 }
 </script>
