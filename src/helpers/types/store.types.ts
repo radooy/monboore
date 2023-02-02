a8120fb34539f447ea554e7ad637b0cbba101fdb
Radostin Kirilov <radostin.kirilov@elando.bg>
2023-02-02 18:28:18 +0200

    add store types

---
+++ src/helpers/types/store.types.ts
@@ -0,0 +1,12 @@
+export type IThemePayload = "light" | "dark";
+
+export interface ILoginPayload {
+  email: string;
+  isAdmin: boolean;
+  uid: string;
+}
+
+export interface IState extends ILoginPayload {
+  theme: IThemePayload;
+  loggedIn: boolean;
+}
