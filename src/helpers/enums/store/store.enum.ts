8debe53efd14c201b0dfc687b7aae2f87839229d
Radostin Kirilov <radostin.kirilov@elando.bg>
2023-02-02 18:27:34 +0200

    Extract store plain text into enum file

---
+++ src/helpers/enums/store/store.enum.ts
@@ -0,0 +1,14 @@
+export enum Mutations {
+  setTheme = "setTheme",
+  setLogIn = "setLogIn",
+}
+
+export enum Actions {
+  updateTheme = "updateTheme",
+  logIn = "logIn",
+}
+
+export enum ThemeVariants {
+  light = "light",
+  dark = "dark",
+}
