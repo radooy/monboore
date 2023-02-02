72f5a3e4a876739567148712eecdf0ff77b85915
Radostin Kirilov <radostin.kirilov@elando.bg>
2023-02-02 18:15:09 +0200

    Created error component

---
+++ src/components/ErrorMessage/ErrorMessage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="text-center text-red-darken-4">
+    {{ messageRef }}
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { defineProps, toRef } from "vue";
+
+const props = defineProps({
+  message: {
+    type: String,
+    default: "",
+  },
+});
+
+const messageRef = toRef(props, "message");
+</script>
