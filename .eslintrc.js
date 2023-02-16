1465800c34abdd1dd66a5d85225d5d88b94902cf
Radostin Kirilov <radostin.kirilov@elando.bg>
2023-02-16 14:45:30 +0200

    Add jest and vue-test-utils

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,23 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
+
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
     "@vue/typescript/recommended",
     "plugin:prettier/recommended",
   ],
+
   parserOptions: {
     ecmaVersion: 2020,
   },
+
   plugins: ["prettier"],
+
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
@@ -24,4 +29,16 @@ module.exports = {
       },
     ],
   },
+
+  overrides: [
+    {
+      files: [
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)",
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
 };
